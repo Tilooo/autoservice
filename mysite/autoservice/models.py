40d0a253972689321c15f5c502392ab6e9deb887
Tilooo <moonaviad@gmail.com>
2023-04-25 15:13:17 +0300

    +Sukurti visus modelius pagal nurodytą programos DB struktūrą

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class VehicleModel(models.Model):
+    make = models.CharField(verbose_name='Gamintojas', max_length=50)
+    model = models.CharField(verbose_name='Modelis', max_length=50)
+
+
+class Service(models.Model):
+    name = models.CharField(verbose_name='Pavadinimas', max_length=50)
+    price = models.IntegerField(verbose_name='Kaina')
+
+
+class vehicle(models.Model):
+    plate = models.CharField(verbose_name='valstybinis numeris', max_length=6)
+    vin = models.CharField(verbose_name='VIN kodas', max_length=17)
+    owner_name = models.CharField(verbose_name='Savininkas', max_length=50)
+    vehicle_model = models.CharField(to='VehicleModel', verbose_name='Automobilio modelis', on_delete=models.SET_NULL, null=True)
+
+
+class Order(models.Model):
+    date = models.DateTimeField(verbose_name='Data', auto_now_add=True)
+    vehicle = models.ForeignKey(to='Vehicle', on_delete=models.SET_NULL, null=True)
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(to='Order', on_delete=models.CASCADE)
+    service = models.ForeignKey(to='Service', verbose_name='Paslauga', on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(verbose_name='Kiekis')
+
+
